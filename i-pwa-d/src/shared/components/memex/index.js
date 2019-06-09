83d16a384a0f859215747e24e9d897209ee43c72
Henry F Ma <henryma@edening.cn>
2019-06-09 09:31:49 +0800

    iMemex: as DWeb.page

--- i-pwa-d/src/shared/components/memex/index.js
+++ i-pwa-d/src/shared/components/memex/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MDBListGroup, MDBListGroupItem, MDBCard, MDBCardBody, MDBModalHeader, MDBInput, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBModal, MDBModalBody, MDBModalFooter, MDBIframe } from "mdbreact";
+import { MDBContainer, MDBBtn, MDBModal, MDBModalHeader, MDBModalBody, MDBModalFooter, MDBIframe } from "mdbreact";
 import styles from './index.module.css'
 
 class Memex extends Component {
@@ -43,37 +43,10 @@ class Memex extends Component {
     return (
       <MDBContainer>
         <MDBBtn onClick={this.toggle(36)}>iMemex</MDBBtn>
-        <MDBModal modalStyle="info" id="imemex" size="lg" isOpen={this.state.modal36} toggle={this.toggle(36)}>
+        <MDBModal modalStyle="info" size="lg" isOpen={this.state.modal36} toggle={this.toggle(36)}>
           <MDBModalHeader>iMemex</MDBModalHeader>
           <MDBModalBody className="mb-0 p-0">
-            <MDBCard>
-              <MDBCardBody>
-                <MDBContainer id="consultBox">
-                  <MDBRow>
-                    <MDBCol md="9">
-                      <MDBInput hint="<trail(s)@iMemex> e.g. memex ipfs ibipfs"  getValue={ this.trails } />
-                    </MDBCol>
-                    <MDBCol md="3">
-                      <MDBBtn onClick={ this.consult }>Consult</MDBBtn>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBContainer>
-                <MDBContainer id="advicesBox">
-                  <MDBListGroup>
-                    <MDBListGroupItem>Memex: Enlarged intimate supplement to our memory...</MDBListGroupItem>
-                    <MDBListGroupItem>IPFS: InterPlanetary File System...</MDBListGroupItem>
-                    <MDBListGroupItem>iMemex: Personal Memex Distributed...</MDBListGroupItem>
-                    <MDBListGroupItem>IBIPFS: In-browser IPFS...</MDBListGroupItem>
-                    <MDBListGroupItem>Trail(s): Index(es) of our human mind...</MDBListGroupItem>
-                    <MDBListGroupItem>Distributed: Nowhere but Everywhere...</MDBListGroupItem>
-                  </MDBListGroup>
-                </MDBContainer>
-                <hr />
-                <MDBContainer id="statsBox">
-                  <MDBIframe className={ styles.container } id="istats" ratio="21by9" scrolling="auto" allowFullScreen title="IPFS Node Stats" src="/stats" />
-                </MDBContainer>
-              </MDBCardBody>
-            </MDBCard>
+            <MDBIframe id="imemex" className={ styles.container } scrolling="auto" allowFullScreen title="Personal Distributed Web" src="https://service.edening.net/ipfs/QmaUT7Sd4wykeL2KLdMixZ9WiMFEdRzZpmhVHWAfLadK2w/" />
           </MDBModalBody>
           <MDBModalFooter className="justify-content-center">
             <MDBBtn color="primary" outline rounded size="md" className="ml-3" onClick={this.toggle(36)}>
